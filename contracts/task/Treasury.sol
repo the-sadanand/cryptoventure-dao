85d13ab5afc92943354ab788f18199ea4fe2bedd
SADANAND <thesadanand.toponed0@gmail.com>
2026-09-16 13:36:41 +0530

    feat: add governance-controlled upgradeable treasury

---
+++ contracts/task/Treasury.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract Treasury is Initializable, UUPSUpgradeable {
+    uint256 public versionNumber;
+
+    function initialize(address governanceExecutor) external initializer {
+        require(governanceExecutor != address(0), "Treasury: zero executor");
+        versionNumber = 1;
+        _transferOwnership(governanceExecutor);
+    }
+
+    function deposit() external payable {}
+
+    function balance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferETH(address payable recipient, uint256 amount) external onlyOwner {
+        require(recipient != address(0), "Treasury: zero recipient");
+        require(amount <= address(this).balance, "Treasury: insufficient balance");
+        (bool ok, ) = recipient.call{value: amount}("");
+        require(ok, "Treasury: transfer failed");
+    }
+
+    function version() external view returns (uint256) {
+        return versionNumber;
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    receive() external payable {}
+}
